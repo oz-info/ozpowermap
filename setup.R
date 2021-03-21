2a889fe87434c76a7f02aa034588ce909f42fc47
dan <github@email.possumpalace.org>
2021-03-21 18:02:34 +1100

    refresh versions

--- setup.R
+++ setup.R
@@ -1,9 +1,6 @@
-URL2PP = "https://results.aec.gov.au/24310/Website/Downloads/HouseTppByDivisionDownload-24310.csv"
-DATAFILENAME = 'HouseTppByDivisionDownload-24310.csv'
-# library(pins)
-# pth = pin(URL_2PP)
-devtools::install_github("ropenscilabs/eechidna", 
-                         build_vignettes = TRUE)
+
+library(eechidna)
+
 
 # nat_map <- nat_map_download(2016)
 # nat_data <- nat_data_download(2016)
